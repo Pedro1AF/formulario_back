07ea36000d71c0f82b0e81abb8e50fc6d219c2ba
Christopher de Mira <159036588+ChristopherVivekananda@users.noreply.github.com>
2025-04-16 09:36:46 -0300

    Add files via upload

--- aaaaa.py
+++ aaaaa.py
@@ -1,24 +1,37 @@
 arq = open('registrados.txt', 'a')
 print('Olá, aqui você pode adicionar uma nova conta!')
 nome_usuario = input('Digite o nome de usuário: ')
+senha_usuario = input('Digite a sua senha: ')
 
-arq.write('{}\n'.format(nome_usuario))
-'''
-Adição da constante \n new line
-Uma vez que write não o adiciona automaticamente
-'''
-
+arq.write(f"{nome_usuario}:{senha_usuario}\n")
 print('Cadastro realizado com sucesso!\n')
-arq.close() #O arquivo é fechado do modo de adição para ser aberto
-            #posteriormente no modo de leitura
-
-arq = open('registrados.txt') #abrindo no modo de leitura
-print('Efetue o seu login')
-nome_login = input('Digite o seu nome de usuario: ')
-
-registrados = arq.readlines()
-if nome_login + '\n' in registrados:
-    print('Bem vindo, {}!'.format(nome_login))
-else:
-    print('Você deve ter digitado seu nome de usuario errado, por favor verifique.')
 arq.close()
+
+arq = open('registrados.txt', 'r')
+registrados = arq.readlines()
+max_tentativas = 3
+tentativas = 0
+login_valido = False
+
+while tentativas < max_tentativas and not login_valido:
+    print('\nEfetue o seu login')
+    nome_login = input('Digite o seu nome de usuario: ')
+    senha_login = input('Digite a sua senha: ')
+
+    for linha in registrados:
+        usuario_armazenado, senha_armazenada = linha.strip().split(':')
+        if nome_login == usuario_armazenado and senha_login == senha_armazenada:
+            login_valido = True
+            break
+
+    if login_valido:
+        print(f'\nBem-vindo, {nome_login}!')
+    else:
+        tentativas += 1
+        restantes = max_tentativas - tentativas
+        print(f'\nUsuário ou senha incorretos. Tentativas restantes: {restantes}')
+
+arq.close()
+
+if not login_valido:
+    print('\nVocê excedeu o número máximo de tentativas. Tente novamente mais tarde.')
